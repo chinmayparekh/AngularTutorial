c25d2a5d6fad346ebd74aaaf6d9c3657ba5a492b
chinmayparekh <chinmayparekh11@gmail.com>
2022-06-09 11:24:51 +0530

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import {UsersModule} from './users/users.module';
 import {FormsModule} from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
